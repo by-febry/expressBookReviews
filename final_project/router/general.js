35b8288c7c2b8066c41a01d570bb0bf01aee60b3
char <126554666+by-febry@users.noreply.github.com>
2025-11-17 09:49:03 +0800

    Implement get books by author endpoint

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,7 +31,22 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const matchingBooks = {};
+  const keys = Object.keys(books);
+  
+  for (let i = 0; i < keys.length; i++) {
+    const isbn = keys[i];
+    if (books[isbn].author === author) {
+      matchingBooks[isbn] = books[isbn];
+    }
+  }
+  
+  if (Object.keys(matchingBooks).length > 0) {
+    return res.status(200).json(matchingBooks);
+  } else {
+    return res.status(404).json({message: "No books found for this author"});
+  }
 });
 
 // Get all books based on title
